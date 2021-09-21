d10988ef264fd6eeac005b3865ce7a4567158718
selivanov.i <selivanov.i@abcloudz.com>
2021-09-21 14:58:13 +0300

    Create request permissions

--- Android. Module 4. Android Components/app/src/main/java/com/example/androidcomponents/MainActivity.kt
+++ Android. Module 4. Android Components/app/src/main/java/com/example/androidcomponents/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.androidcomponents
 
+import android.Manifest
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermission()
+    }
+
+
+    private fun hasReadExternalStoragePermissions() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        val permission = mutableListOf<String>()
+
+        if(!hasReadExternalStoragePermissions())
+            permission.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+        if(permission.size != 0)
+            ActivityCompat.requestPermissions(this, permission.toTypedArray(), 0)
+
     }
 }
