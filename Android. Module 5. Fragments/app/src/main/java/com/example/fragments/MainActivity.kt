42e8f9ef0681bc5e18ec9314d79feed20c93ecc1
Ilya <selivanov.i@abcloudz.com>
2021-09-29 02:07:43 +0300

    Create navigation between main and settings

--- Android. Module 5. Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Android. Module 5. Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -5,22 +5,40 @@ import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private val navigationView: NavigationView by lazy {
+        findViewById(R.id.navigationView)
+    }
 
+    private val drawerLayout: DrawerLayout by lazy {
+        findViewById(R.id.drawerLayout)
+    }
+
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment)
+            .navController
+    }
+
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        AppBarConfiguration(navController.graph, drawerLayout)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        val navController: NavController = findNavController(R.id.containerView)
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-
+        navigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfiguration) ||
+                super.onSupportNavigateUp()
 }
