c381d9b58b6c613741dceca9b1b8283bd9cf0de2
selivanov.i <selivanov.i@abcloudz.com>
2021-09-28 18:01:49 +0300

    Create navigation

--- Android. Module 5. Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Android. Module 5. Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navController: NavController = findNavController(R.id.containerView)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 }
---
+++ Android. Module 5. Fragments/app/src/main/java/com/example/fragments/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.example.fragments
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+}
