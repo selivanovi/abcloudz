6e24542fd8784c690e72f1de0bd5b73937fed434
Ilya <selivanov.i@abcloudz.com>
2021-10-14 00:17:02 +0300

    Add log into CancelReceiver and add receiver into AndroidManifest

--- Android. Module 6. Notifications/app/src/main/java/com/example/notifications/CancelReceiver.kt
+++ Android. Module 6. Notifications/app/src/main/java/com/example/notifications/CancelReceiver.kt
@@ -3,10 +3,14 @@ package com.example.notifications
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class CancelReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
+
+        Log.d("MyApp", "onReceive")
+
         if (NotificationHelper.ACTION_CANCEL == intent?.action){
             val itemId = intent.getIntExtra(NotificationHelper.KEY_ITEM_ID, 0)
 
