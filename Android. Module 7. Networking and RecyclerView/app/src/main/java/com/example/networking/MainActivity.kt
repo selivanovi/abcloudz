780da230b9a5976d561d714cf7cc2a7ad1caf057
Ilya <selivanov.i@abcloudz.com>
2021-10-19 22:43:43 +0300

    Get list of characters in MainActivity.kt

--- Android. Module 7. Networking and RecyclerView/app/src/main/java/com/example/networking/MainActivity.kt
+++ Android. Module 7. Networking and RecyclerView/app/src/main/java/com/example/networking/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.networking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.networking.model.Character
+import com.example.networking.model.CharactersList
+import com.example.networking.network.NetworkLayer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NetworkLayer.apiService.getCharacters().enqueue(object : Callback<CharactersList> {
+            override fun onResponse(
+                call: Call<CharactersList>,
+                response: Response<CharactersList>
+            ) {
+                Log.i("MainActivity", "List size: ${response.body()?.charactersList?.size}")
+            }
+
+            override fun onFailure(call: Call<CharactersList>, t: Throwable) {
+                Log.i("MainActivity", t.message.toString())
+            }
+
+        })
     }
 }
