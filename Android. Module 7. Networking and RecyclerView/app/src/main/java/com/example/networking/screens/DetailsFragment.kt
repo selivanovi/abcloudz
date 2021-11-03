a0152a918ec623b50c8dff387e65260527f90b0b
selivanov.i <selivanov.i@abcloudz.com>
2021-11-03 14:41:17 +0200

    Create EpisodeOffsetItemDecoration.kt

--- Android. Module 7. Networking and RecyclerView/app/src/main/java/com/example/networking/screens/DetailsFragment.kt
+++ Android. Module 7. Networking and RecyclerView/app/src/main/java/com/example/networking/screens/DetailsFragment.kt
@@ -8,14 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.networking.EpisodeLayoutManager
 import com.example.networking.R
 import com.example.networking.model.Character
+import com.example.networking.recyclerview.EpisodeOffsetItemDecoration
 import com.example.networking.recyclerview.EpisodesAdapter
-import com.example.networking.recyclerview.decorators.EpisodeOffsetItemDecoration
 import com.example.networking.setImageFromUrl
 import com.example.networking.viewmodel.DetailsViewModel
 import kotlinx.coroutines.flow.collect
@@ -55,12 +54,11 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     private fun createRecyclerView(view: View, character: Character) {
         val adapter = EpisodesAdapter()
         val recyclerView = view.findViewById<RecyclerView>(R.id.episodesRecyclerView)
-//        val snapHelper = PagerSnapHelper()
-//        snapHelper.attachToRecyclerView(recyclerView)
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = EpisodeLayoutManager()
 
-//        LinearLayoutManager()
         recyclerView.addItemDecoration(EpisodeOffsetItemDecoration(25, 25, 25, 25))
 
         lifecycleScope.launch {
