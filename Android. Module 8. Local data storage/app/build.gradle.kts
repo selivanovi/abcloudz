fb08f894e6ea1d2b04efc4da571d1b59cd5ff61f
selivanov.i <selivanov.i@abcloudz.com>
2021-11-04 15:24:17 +0200

    Move to Gradle.kts

---
+++ Android. Module 8. Local data storage/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.example.localdatastorage"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile ("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility  = JavaVersion.VERSION_1_8
+        targetCompatibility =  JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+
+
+dependencies {
+
+    implementation(Dependencies.Android.coreKtx)
+    implementation(Dependencies.Android.appCompat)
+    implementation(Dependencies.Android.material)
+    implementation(Dependencies.Android.constraintLayout)
+
+    testImplementation(Dependencies.Test.jUnit)
+    androidTestImplementation(Dependencies.Test.androidJUnit)
+    androidTestImplementation(Dependencies.Test.espresso)
+
+    
+}
---
+++ Android. Module 8. Local data storage/buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,15 @@
+object Dependencies {
+
+    object Android {
+        const val coreKtx = "androidx.core:core-ktx:1.6.0"
+        const val appCompat = "androidx.appcompat:appcompat:1.3.1"
+        const val material = "com.google.android.material:material:1.4.0"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.1"
+    }
+
+    object Test {
+        const val jUnit = "junit:junit:4.+"
+        const val androidJUnit = "androidx.test.ext:junit:1.1.3"
+        const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+    }
+}
