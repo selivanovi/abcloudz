222f33fd8d3466654d5626eaecf4d28dd2341ebc
selivanov.i <selivanov.i@abcloudz.com>
2021-11-25 17:32:45 +0200

    Created MainScreen

--- Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/MainActivity.kt
+++ Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.camera
 
+import android.content.res.Resources
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.index)
+        Log.d("MainActivity", "ImageView: width - ${imageView.measuredWidth}, height - ${imageView.measuredHeight}")
     }
 }
