eaad851a87f6d3374a74a518341a00cb89eedee4
selivanov.i <selivanov.i@abcloudz.com>
2021-11-26 15:46:35 +0200

    Created activity result for getting photo from file

--- Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/MainActivity.kt
+++ Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/MainActivity.kt
@@ -2,18 +2,37 @@ package com.example.camera
 
 import android.content.res.Resources
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
+import com.example.camera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val getContent: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri: Uri? ->
+            binding.imageView.load(imageUri)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageResource(R.drawable.index)
-        Log.d("MainActivity", "ImageView: width - ${imageView.measuredWidth}, height - ${imageView.measuredHeight}")
+        binding.takePhotoButton.setOnClickListener {
+            getContent.launch(MIMETYPE_IMAGES)
+        }
+    }
+
+    companion object {
+        private const val MIMETYPE_IMAGES = "image/*"
     }
 }
