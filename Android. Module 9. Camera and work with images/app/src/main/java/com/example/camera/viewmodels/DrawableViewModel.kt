409c32b3e3cb59429d4193d663c1dea89bb5abe1
selivanov.i <selivanov.i@abcloudz.com>
2021-12-09 16:29:12 +0200

    Deleted fun emitOriginalBitmap and cleared imports in viewModel

--- Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/viewmodels/DrawableViewModel.kt
+++ Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/viewmodels/DrawableViewModel.kt
@@ -13,17 +13,11 @@ import androidx.lifecycle.*
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
-import androidx.annotation.RequiresApi
-import com.example.camera.Constants
-import com.example.camera.R
+
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
 import java.io.OutputStream
-import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.util.*
+
 
 
 class DrawableViewModel(context: Application) : AndroidViewModel(context) {
@@ -65,9 +59,6 @@ class DrawableViewModel(context: Application) : AndroidViewModel(context) {
         _channelAddable.value = boolean
     }
 
-    fun emitOriginalBitmap() {
-        _channelImageBitmap.value = originalBitmap
-    }
 
     fun getBitmapFromUri(imageUri: Uri): Bitmap? {
         getApplication<Application>().contentResolver.openInputStream(imageUri)?.let {
@@ -141,36 +132,20 @@ class DrawableViewModel(context: Application) : AndroidViewModel(context) {
 
     fun saveBitmapToStorage(bitmap: Bitmap) {
         val context = getApplication<Application>()
-        //Generating a file name
         val filename = "${System.currentTimeMillis()}.jpg"
-
-        //Output stream
         var fos: OutputStream? = null
-
-        //For devices running android >= Q
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            //getting the contentResolver
             context.contentResolver?.also { resolver ->
-
-                //Content resolver will process the contentvalues
                 val contentValues = ContentValues().apply {
-
-                    //putting file information in content values
                     put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
                     put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
                     put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
                 }
-
-                //Inserting the contentValues to contentResolver and getting the Uri
                 val imageUri: Uri? =
                     resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-                //Opening an outputstream with the Uri that we got
                 fos = imageUri?.let { resolver.openOutputStream(it) }
             }
         } else {
-            //These for devices running on android < Q
-            //So I don't think an explanation is needed here
             val imagesDir =
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
             val image = File(imagesDir, filename)
@@ -178,7 +153,6 @@ class DrawableViewModel(context: Application) : AndroidViewModel(context) {
         }
 
         fos?.use {
-            //Finally writing the bitmap to the output stream that we opened
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
         }
     }
