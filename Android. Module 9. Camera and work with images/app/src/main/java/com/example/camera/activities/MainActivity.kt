7cfd485d9f213f933371653abc129efcd8431fd9
Ilya <selivanov.i@abcloudz.com>
2021-12-10 16:46:44 +0200

    Removed binding in MainActivity

--- Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/activities/MainActivity.kt
+++ Android. Module 9. Camera and work with images/app/src/main/java/com/example/camera/activities/MainActivity.kt
@@ -4,23 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import com.example.camera.R
-import com.example.camera.databinding.ActivityMainBinding
 import com.example.camera.fragments.PhotoFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add(R.id.mainContainerView, PhotoFragment(), null)
         }
-
     }
 }
