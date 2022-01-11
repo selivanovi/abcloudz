f40ccc321eb9cb6e090ac22372534a277c234982
selivanov.i <selivanov.i@abcloudz.com>
2022-01-11 18:25:40 +0200

    Create SplashScreen

---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/SplashScreen.kt
@@ -0,0 +1,15 @@
+package com.example.spyfall.screen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+import com.example.spyfall.screen.customview.OutlineTextView
+
+class SplashScreen : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
