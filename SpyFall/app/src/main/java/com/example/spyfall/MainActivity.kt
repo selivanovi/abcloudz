e85f7680ac71003896b564f5c81875a7a404c728
selivanov.i <selivanov.i@abcloudz.com>
2022-01-12 20:18:56 +0200

    Create DrawerLayout

--- SpyFall/app/src/main/java/com/example/spyfall/MainActivity.kt
+++ SpyFall/app/src/main/java/com/example/spyfall/MainActivity.kt
@@ -1,11 +1,110 @@
 package com.example.spyfall
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.forEach
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.spyfall.dialog.QuiteDialog
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val drawerLayout by lazy {
+        findViewById<DrawerLayout>(R.id.drawerLayout)
+    }
+
+    private val navigationView by lazy {
+        findViewById<NavigationView>(R.id.navigation_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createDrawerLayout()
+
+        createNavigationView()
+
+
+    }
+
+    private fun createDrawerLayout() {
+
+        val content = findViewById<CardView>(R.id.cardContainer)
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+
+                val diffScaledOffset: Float = slideOffset * (1 - END_SCALE)
+                val offsetScale = 1 - diffScaledOffset
+                content.scaleX = offsetScale
+                content.scaleY = offsetScale
+
+                val diffCornerRadius: Float =
+                    slideOffset * (1 - resources.getDimension(R.dimen.corner_radius_main_container))
+                val offsetRadius = 1 - diffCornerRadius
+                content.radius = offsetRadius
+
+                val diffCardElevation: Float =
+                    slideOffset * (1 - resources.getDimension(R.dimen.elevation_main_container))
+                val offsetElevation = 1 - diffCardElevation
+                content.cardElevation = offsetElevation
+
+                val xOffset = drawerView.width * slideOffset
+                val xOffsetDiff: Float = content.width * diffScaledOffset / 2
+                val xTranslation = xOffset - xOffsetDiff
+                content.translationX = -xTranslation
+            }
+        })
+        drawerLayout.drawerElevation = 0f
+    }
+
+    private fun createNavigationView() {
+
+        navigationView.getHeaderView(0).findViewById<ImageView>(R.id.closeImageView)
+            .setOnClickListener {
+                drawerLayout.closeDrawer(navigationView)
+            }
+
+        navigationView.setNavigationItemSelectedListener {
+
+            navigationView.menu.forEach { item -> item.isChecked = false }
+
+            it.isChecked = true
+
+            when (it.itemId) {
+                R.id.itemCreateGame -> {
+                }
+                R.id.itemAbout -> {
+                }
+                R.id.itemRules -> {
+                }
+                R.id.itemGoOut -> QuiteDialog().show(supportFragmentManager, null)
+            }
+
+            true
+        }
+    }
+
+    /**
+    Replace with callback
+     */
+    fun moveDrawableLayout() {
+        if (!drawerLayout.isDrawerOpen(navigationView)) {
+            drawerLayout.openDrawer(navigationView)
+        } else {
+            drawerLayout.closeDrawer(navigationView)
+        }
+    }
+
+    companion object {
+        private const val END_SCALE = 0.7f
     }
 }
--- SpyFall/app/src/main/java/com/example/spyfall/screen/EnterNameScreen.kt
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/EnterNameScreen.kt
@@ -1,7 +1,23 @@
 package com.example.spyfall.screen
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.example.spyfall.MainActivity
 import com.example.spyfall.R
 
 class EnterNameScreen : Fragment(R.layout.fragment_enter_name) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menu = view.findViewById<ImageView>(R.id.menu)
+
+        menu.setOnClickListener {
+
+            (requireActivity() as MainActivity).moveDrawableLayout()
+
+        }
+    }
 }
