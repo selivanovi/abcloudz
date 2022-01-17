06b407e8e678ccbea5726f1c19402ee24340adb5
Ilya <selivanov.i@abcloudz.com>
2022-01-17 14:24:47 +0200

    Create StartGameScreen

---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/StartGameScreen.kt
@@ -0,0 +1,7 @@
+package com.example.spyfall.screen
+
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+
+class StartGameScreen : Fragment(R.layout.fragment_start_game) {
+}
---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/view/JoinGameView.kt
@@ -0,0 +1,7 @@
+package com.example.spyfall.screen.view
+
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+
+class JoinGameView : Fragment(R.layout.fragment_join_game) {
+}
