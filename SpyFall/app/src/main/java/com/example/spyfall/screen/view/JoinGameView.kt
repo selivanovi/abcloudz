315af665bcb5de1d78f9ad439a2bcaab97ce02e5
selivanov.i <selivanov.i@abcloudz.com>
2022-01-18 19:55:58 +0200

    Create JoinGameView

--- SpyFall/app/src/main/java/com/example/spyfall/screen/view/JoinGameView.kt
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/view/JoinGameView.kt
@@ -3,5 +3,5 @@ package com.example.spyfall.screen.view
 import androidx.fragment.app.Fragment
 import com.example.spyfall.R
 
-class JoinGameView : Fragment(R.layout.fragment_join_game) {
+class JoinGameView : Fragment(R.layout.fragment_join_game_view) {
 }
