489edd5c2bb25cab161ea6f6776613dba583b4e7
selivanov.i <selivanov.i@abcloudz.com>
2022-01-18 19:57:50 +0200

    Create array with different time

---
+++ SpyFall/app/src/main/java/com/example/spyfall/utils/Time.kt
@@ -0,0 +1,19 @@
+package com.example.spyfall.utils
+
+import com.example.spyfall.R
+
+data class Time(val value: Int, val measure: Int)
+
+val times = listOf<Time>(
+    Time(5, R.string.measure_of_time),
+    Time(6, R.string.measure_of_time),
+    Time(7, R.string.measure_of_time),
+    Time(8, R.string.measure_of_time),
+    Time(9, R.string.measure_of_time),
+    Time(10, R.string.measure_of_time),
+    Time(11, R.string.measure_of_time),
+    Time(12, R.string.measure_of_time),
+    Time(13, R.string.measure_of_time),
+    Time(14, R.string.measure_of_time),
+    Time(15, R.string.measure_of_time),
+)
