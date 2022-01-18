ed9dd1260535a3429582ef64c120b29ca7f55ee0
selivanov.i <selivanov.i@abcloudz.com>
2022-01-18 19:53:39 +0200

    Create CheckLocationScreen

---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/CheckLocationScreen.kt
@@ -0,0 +1,8 @@
+package com.example.spyfall.screen
+
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+
+class CheckLocationScreen : Fragment(R.layout.fragment_check_location) {
+
+}
