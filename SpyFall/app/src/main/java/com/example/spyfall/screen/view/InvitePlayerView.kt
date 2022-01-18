ed30b3b175b842e558c00d322c3314cda41be4bb
selivanov.i <selivanov.i@abcloudz.com>
2022-01-18 19:52:04 +0200

    Create InvitePlayerView

---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/view/InvitePlayerView.kt
@@ -0,0 +1,7 @@
+package com.example.spyfall.screen.view
+
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+
+class InvitePlayerView : Fragment(R.layout.fragment_invite_player) {
+}
