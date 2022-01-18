6f7deee7ecbddac6dbdb509a57af4296e794034c
selivanov.i <selivanov.i@abcloudz.com>
2022-01-18 19:52:57 +0200

    Create ResultScreen

---
+++ SpyFall/app/src/main/java/com/example/spyfall/screen/ResultFragment.kt
@@ -0,0 +1,17 @@
+package com.example.spyfall.screen
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.spyfall.R
+
+class ResultFragment : Fragment(R.layout.fragment_result) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.locationButton).setOnClickListener {
+        }
+    }
+}
