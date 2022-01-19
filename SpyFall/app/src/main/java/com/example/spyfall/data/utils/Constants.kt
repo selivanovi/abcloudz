7bbfa36d83555f27ae8df2e8c9ce06e40c263bca
selivanov.i <selivanov.i@abcloudz.com>
2022-01-19 19:28:05 +0200

    Add constants with messages of exceptions

---
+++ SpyFall/app/src/main/java/com/example/spyfall/data/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.spyfall.data.utils
+
+object Constants {
+
+    internal const val GET_DATA_EXCEPTION = "Something happened, can't get data"
+}
---
+++ SpyFall/app/src/main/java/com/example/spyfall/domain/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.spyfall.domain.utils
+
+object Constants {
+
+    internal const val INVALID_NAME_EXCEPTION = "This name is already in use"
+    internal const val GET_DATA_EXCEPTION = "Something happened, can't get data"
+}
