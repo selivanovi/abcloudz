aaa3ac81159ee2a597e23115f6acf74459badcc4
selivanov.i <selivanov.i@abcloudz.com>
2022-02-03 20:22:32 +0200

    Create CheckLocationViewModel

---
+++ SpyFall/app/src/main/java/com/example/spyfall/ui/viewmodel/CheckLocationViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.spyfall.ui.viewmodel
+
+import com.example.spyfall.data.entity.GameStatus
+import com.example.spyfall.domain.repository.GameRepository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CheckLocationViewModel @Inject constructor(
+    private val gameRepository: GameRepository
+) : BaseViewModel() {
+
+    private val checkStateMutableChannel = Channel<CheckState>()
+    val checkStateChannel = checkStateMutableChannel.receiveAsFlow()
+
+    fun observeGame(gameId: String) {
+            launch {
+                gameRepository.observeGame(gameId).collect { result ->
+                    result.onSuccess { game ->
+
+                        if (game.status != null) {
+                            if (game.status == GameStatus.SPY_WON)
+                                checkStateMutableChannel.send(CheckState.SpyWon)
+                            if (game.status == GameStatus.LOCATION_WON)
+                                checkStateMutableChannel.send(CheckState.SpyLost)
+                        }
+                    }
+                    result.onFailure { throwable ->
+                        errorMutableChannel.send(throwable)
+                    }
+                }
+            }
+    }
+
+    fun setStatusForGame(gameId: String, status: GameStatus) {
+        launch {
+            gameRepository.setStatusForGame(gameId, status)
+        }
+    }
+}
+
+sealed class CheckState {
+
+    object SpyWon : CheckState()
+    object SpyLost : CheckState()
+}
